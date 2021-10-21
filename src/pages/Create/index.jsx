eabbb4f726f052b3acf5fdb1ef5be79f7886cf13
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:57:02 -0500

    Create boilerplate for create page

---
+++ src/pages/Create/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import styles from "./Create.module.css";
+
+const Create = () => {
+  return (
+    <main>
+      <Header title="Publicar" />
+    </main>
+  );
+};
+
+export default Create;
