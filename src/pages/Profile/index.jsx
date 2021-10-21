9ba6234defa44e02c078850e4d75b3a53325188c
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:56:45 -0500

    Create profile boilerplate

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import styles from "./Profile.module.css";
+
+const Profile = () => {
+  return (
+    <main>
+      <Header title="Perfil" />
+    </main>
+  );
+};
+
+export default Profile;
