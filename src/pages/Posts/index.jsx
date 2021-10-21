275cdee7174c3daf3ea6a90ac0443feab10740c3
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 18:04:01 -0500

    Add a redirect button to /create at my posts page

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,10 +1,16 @@
+import Button from "../../components/Button";
 import Header from "../../components/Header";
 import styles from "./Posts.module.css";
+import newPost from "./newPost.svg";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
   return (
     <main>
       <Header title="Mis posts" />
+      <Link className={styles.link} to="/create">
+        <Button text="Crear post" icon={newPost} />
+      </Link>
     </main>
   );
 };
