0feab19b2a26f58d075af0d804cfa6939b0fb181
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:18:53 -0500

    Remove header tag from Layout component

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -3,7 +3,6 @@ import styles from "./Layout.module.css";
 const Layout = ({ children }) => (
   <div className={styles.container}>
     <div className={styles.content}>
-      <header></header>
       <>{children}</>
       <footer></footer>
     </div>
