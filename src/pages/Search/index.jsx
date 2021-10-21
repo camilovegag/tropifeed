00cc7802d2138de93261e9df67d2221952d21021
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:56:29 -0500

    Create search boilerplate

---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import styles from "./Search.module.css";
+
+const Search = () => {
+  return (
+    <main>
+      <Header title="Buscar" />
+    </main>
+  );
+};
+
+export default Search;
