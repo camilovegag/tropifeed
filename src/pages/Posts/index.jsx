0e0692f9ff40432cf9f6e261521194c8c806bedd
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:18:18 -0500

    Create posts boilerplate

---
+++ src/pages/Posts/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import styles from "./Posts.module.css";
+
+const Posts = () => {
+  return (
+    <main>
+      <Header title="Mis posts" />
+    </main>
+  );
+};
+
+export default Posts;
