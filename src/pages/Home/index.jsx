bdfeb165af7fd86f1ad6c8305e2b1b925000bebd
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 11:10:22 -0500

    Create home boilerplate

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Card";
+import styles from "./Home.module.css";
+
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("data.json");
+    const feedData = await response.json();
+    setData(feedData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <main>
       <h1>Inicio</h1>
       <hr />
+      <section className={styles.cards}>
+        {data.map((data, idx) => (
+          <Card
+            key={idx}
+            imgLink={data.imgLink}
+            title={data.title}
+            description={data.description}
+          />
+        ))}
+      </section>
     </main>
   );
 };
