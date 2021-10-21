d28edf405011b8f39452a6f9e27e21764ed1b4c6
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 13:22:37 -0500

    Add layout to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Layout from "./Layout";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
+    <Layout>
       <Home />
-    </div>
+    </Layout>
   );
 };
 
