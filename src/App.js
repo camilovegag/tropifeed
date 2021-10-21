d050b8022269c8c375d8f574a1a0a3a6f5512d34
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 15:57:11 -0500

    Add app routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import Layout from "./Layout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Create from "./pages/Create";
 import Home from "./pages/Home";
+import Layout from "./Layout";
+import Posts from "./pages/Posts";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
     <Layout>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/posts">
+            <Posts />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
     </Layout>
   );
 };
