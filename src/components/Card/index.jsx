c4b7048fe2ac08c3ee1951078729841550d8d165
camilovegag <camilovegag@unisabana.edu.co>
2021-10-21 13:21:23 -0500

    Fix card image

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,9 @@ import styles from "./Card.module.css";
 const Card = ({ imgLink, title, description }) => {
   return (
     <article className={styles.card}>
-      <img className={styles.image} src={imgLink} alt="test" />
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={imgLink} alt="Producto" />
+      </div>
       <h6 className={styles.title}>{title}</h6>
       <p className={styles.description}>{description}</p>
     </article>
