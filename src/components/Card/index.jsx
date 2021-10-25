e13092109a3ec12128c821074bbc92ced2d970e1
camilovegag <camilovegag@unisabana.edu.co>
2021-10-25 16:51:56 -0500

    Add icons to delete and edit

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,30 @@
 import styles from "./Card.module.css";
+import editIcon from "./edit.svg";
+import deleteIcon from "./delete.svg";
 
-const Card = ({ link, title, description }) => {
+const Card = ({ editable, link, title, description, onClickEdit, onClickDelete }) => {
   return (
     <article className={styles.card}>
       <div className={styles.imageContainer}>
-        <img className={styles.image} src={link} alt="Producto" />
+        {editable ? (
+          <>
+            <img
+              className={`${styles.edit} ${styles.icon}`}
+              src={editIcon}
+              alt="edit"
+              onClick={onClickEdit}
+              title="Editar post"
+            />
+            <img
+              className={`${styles.delete} ${styles.icon}`}
+              src={deleteIcon}
+              alt="delete"
+              onClick={onClickDelete}
+              title="Eliminar post"
+            />
+          </>
+        ) : null}
+        <img className={styles.image} src={link} alt="" />
       </div>
       <h6 className={styles.title}>{title}</h6>
       <p className={styles.description}>{description}</p>
