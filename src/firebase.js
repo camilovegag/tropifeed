0db8e28b2c55213f94f12eb2e88d59c633166252
camilovegag <camilovegag@unisabana.edu.co>
2021-10-25 16:51:41 -0500

    Add firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAh030hzIfRLLuWhsp4bo23bKWlY0zFZ5M",
   authDomain: "tropifeed.firebaseapp.com",
@@ -17,4 +15,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
 export default db;
