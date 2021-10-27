ba7a2cfb7a935c326144692712cc6f0b2297e501
camilovegag <camilovegag@unisabana.edu.co>
2021-10-27 11:33:45 -0500

    Modify header component to recive icons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 
-const Header = ({ title }) => (
+const Header = ({ icon, title }) => (
   <header>
-    <h1>{title}</h1>
+    <div className={styles.headerContainer}>
+      {icon ? <img src={icon} alt="Icon" /> : null}
+      <h1>{title}</h1>
+    </div>
     <hr className={styles.divider} />
   </header>
 );
