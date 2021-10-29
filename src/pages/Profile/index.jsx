abe50dccf2151c57b53ece264016f1880a3cb666
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2021-10-29 02:17:11 -0500

    Implement logout function

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,29 @@
 import Header from "../../components/Header";
 import styles from "./Profile.module.css";
+import { useAuth } from "../../firebase.js";
+import Button from '../../components/Button'
+import { logOut } from "../../firebase";
+import {useHistory} from 'react-router-dom';
 
 const Profile = () => {
+  const currentUser = useAuth();
+  const history = useHistory();
+
+  const signOut = async () => {
+    try {
+      await logOut()
+      history.push('/signin')
+  
+    } catch  {
+      alert('An error ocurred while trying to log out')
+    }
+  }
+
   return (
     <main>
       <Header title="Perfil" />
+      <p>{currentUser?.email}</p>
+      <Button text="Cerrar sesión" onClick={signOut} />
     </main>
   );
 };
