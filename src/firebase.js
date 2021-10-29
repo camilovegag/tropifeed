ca0bed73b793c42d1071076856f417209941a759
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2021-10-29 02:17:00 -0500

    Create custom useAuth hook & logout function

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import {useState, useEffect} from 'react';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAh030hzIfRLLuWhsp4bo23bKWlY0zFZ5M",
@@ -20,5 +21,21 @@ const auth = getAuth(app);
 export function signUp(email, password) {
   return createUserWithEmailAndPassword(auth, email, password);
 }
+
+export function logOut(){
+  return signOut(auth)
+}
+
+export function useAuth(){
+  const [currentUser, setCurrentUser] = useState();
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+    return unsub;
+  }, [])
+  return currentUser;
+}
+
 
 export default db;
